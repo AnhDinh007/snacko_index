002020ccea656bca9f1b714597f8a25fa399bece
AnhDinh007 <anhdinh.31221026957@st.ueh.edu.vn>
2025-05-29 13:13:13 +0700

    Add files via upload

---
+++ products.php
@@ -0,0 +1,19 @@
+<?php
+$products = [
+    1 => [
+        "name" => "Bánh Gấu Matcha",
+        "price" => 10000,
+        "image" => "images/banh-gau-matcha.jpg",
+    ],
+    2 => [
+        "name" => "Hạt Sấy & Đậu Mix",
+        "price" => 7000,
+        "image" => "images/hat-say-va-dau-mix.jpg", // ✅ Đã đổi tên
+    ],
+    3 => [
+        "name" => "T-Rexx Vị Vải",
+        "price" => 5000,
+        "image" => "images/nuoc-vai.jpg",
+    ],
+];
+?>
